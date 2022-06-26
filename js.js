19d84b73cf6bf4d838e7e95e559c8e650b3200c6
Bernardo Curso <bernardo-v.f@hotmail.com>
2022-06-26 19:32:32 -0300

    scroll suave

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-
+//linha aumentar ao entrar na pag
 function aumentarLinha(){
  document.querySelectorAll('.linha-sobre').forEach(item =>{
    item.style.width = '100%'
@@ -6,3 +6,31 @@ function aumentarLinha(){
 }
 
 setTimeout(aumentarLinha,500)
+
+//scroll suave botao voltar
+
+
+function subirTela(){
+  window.scrollTo({
+    left:0,
+    top:0,
+    behavior:'smooth'   
+  })
+}
+
+function aparecerBotaoScroll(){
+  let verifica = window.scrollY
+  let botao = document.querySelector('.bolaScroll')
+
+  if(verifica <= 450){
+    botao.classList.add('ativo')
+  }else{
+    botao.classList.remove('ativo')
+  }
+  console.log(verifica)
+ 
+}
+
+window.addEventListener('scroll', aparecerBotaoScroll)
+
+
